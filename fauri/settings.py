7a745bed5d0ec07f5263699027200d1c78bc886d
Pceolka <jenia.zspataru@gmail.com>
2023-11-14 15:56:13 +0200

    V3.9.3hotfix2

--- fauri/settings.py
+++ fauri/settings.py
@@ -29,10 +29,10 @@ DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = ["*"]
 
-DATABASE_URL = "mysql://root:2-g2Cg-G1FHa6-bGfCBdgGg1DbCfDe5C@mysql.railway.internal:3306/railway"
+DATABASE_URL = "mysql://root:2-g2Cg-G1FHa6-bGfCBdgGg1DbCfDe5C@monorail.proxy.rlwy.net:55573/railway"
 # Конфигурация базы данных Django
 DATABASES = {
-    'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
 #     'ENGINE': 'django.db.backends.sqlite3',
 #      'NAME': BASE_DIR / 'datebase.db',
